83253986843d6b766f02618429eac3c2cd4a1330
mottakin <md.mottakin.chowdhury@gmail.com>
2018-01-27 18:39:41 +0600

    added camera and image saving

--- app/src/main/java/com/life/mottakin/daruchiniocr/daruchiniMainActivity.java
+++ app/src/main/java/com/life/mottakin/daruchiniocr/daruchiniMainActivity.java
@@ -1,33 +1,114 @@
 package com.life.mottakin.daruchiniocr;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static java.lang.Thread.sleep;
 
 public class daruchiniMainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-//    static {
-//        System.loadLibrary("native-lib");
-//    }
+    static final int REQUEST_IMAGE_CAPTURE=1;
+    static final int REQUEST_EXTERNAL_STORAGE_RESULT=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daruchini_main);
-
-        // Example of a call to a native method
-//        TextView tv = (TextView) findViewById(R.id.sample_text);
-//        tv.setText(stringFromJNI());
+        takePhoto();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-//    public native String stringFromJNI();
+    public void takePhoto() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
+            launchCamera();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "Would you allow Daruchini to user external storage?",
+                            Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_EXTERNAL_STORAGE_RESULT);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==REQUEST_EXTERNAL_STORAGE_RESULT) {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            } else {
+                Toast.makeText(this,
+                        "External storage permission is denied - cannot save images locally.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
+    }
+
+    private void launchCamera() {
+
+        ImageButton cameraButton=findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent,REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode==REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK) {
+            Bundle extras=data.getExtras();
+            Bitmap capturedPhoto=(Bitmap)extras.get("data");
+            saveImage(capturedPhoto,"test.png");
+        }
+
+    }
+
+    private void saveImage(Bitmap imageToSave, String filename) {
+
+        String root=Environment.getExternalStorageDirectory().toString();
+        File daruchiniDirectory=new File(root+"/daruchini-ocr");
+        daruchiniDirectory.mkdirs();
+
+        File file=new File(daruchiniDirectory,filename);
+
+        if(file.exists()) file.delete();
+
+        try {
+            FileOutputStream out=new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.PNG,100,out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
